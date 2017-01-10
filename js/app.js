e988a90e5f4e91b78038508850d8d33633408883
sowjanyamanugula <manugulasowjanya@gmail.com>
2017-01-10 14:19:07 +0530

    Frontend Development Session5 Assignment4 code
    Frontend Development Session5 Assignment4 code

---
+++ js/app.js
@@ -0,0 +1,48 @@
+ var Employee=[{
+        Name:"Johny",
+        age:25,
+        salary:"6 lakhs p/a",
+        address:{
+                  city:"Hyderabad",
+                  State:"Telangana",
+                  Pincode:"500084"
+        }
+ },{
+        Name:"James",
+        age:21,
+        salary:"3 lakhs p/a",
+        address:{
+                  city:"Kolkatta",
+                  State:"WestBengal",
+                  Pincode:"721304"
+        }
+ },{
+        Name:"Mark",
+        age:23,
+        salary:"4 lakhs p/a",
+        address:{
+                  city:"Bangalore",
+                  State:"Karnataka",
+                  Pincode:"560025"
+        }
+ },{
+        Name:"Ceaser",
+        age:35,
+        salary:"12 lakhs p/a",
+        address:{
+                  city:"Chennai",
+                  State:"Tamil nadu",
+                  Pincode:"600040"
+        }
+ },{
+        Name:"Mohan",
+        age:30,
+        salary:"10 lakhs p/a",
+        address:{
+                  city:"Hyderabad",
+                  State:"Telangana",
+                  Pincode:"500084"
+        }
+ }];
+ 
+        console.log(Employee[2]);
